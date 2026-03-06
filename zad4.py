c60e44c83276289a7df58272a8efb59673633268
unknown <kbanaszewskaa@gmail.com>
2026-03-06 12:28:45 +0100

    Added Add functionality

--- zad4.py
+++ zad4.py
@@ -1,1 +1,10 @@
 from abc import ABC, abstractmethod
+
+class IArithmeticsAdd(ABC):
+    @abstractmethod
+    def addition(self, A: float, B: float) -> float:
+        pass
+
+class ArithmeticAdd(IArithmeticsAdd):
+    def addition(self, A: float, B: float) -> float:
+        return A + B
