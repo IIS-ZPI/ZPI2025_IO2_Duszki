637e43b758c622d650354689030e4ae9de64782a
Wojciech Korbel <251169@edu.p.lodz.pl>
2026-03-06 12:27:12 +0100

    Added IArithmeticsDiv

--- zad4.py
+++ zad4.py
@@ -1,1 +1,14 @@
 from abc import ABC, abstractmethod
+
+class IArithmeticsDiv(ABC):
+
+    @abstractmethod
+    def division(self, A: float, B: float) -> float:
+        pass
+
+class ArithmeticDiv(IArithmeticsDiv):
+
+    def division(self, A: float, B: float) -> float:
+        if B == 0:
+            raise ValueError("Division by zero")
+        return A / B
