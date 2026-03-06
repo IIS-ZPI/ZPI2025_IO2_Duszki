6e90973e3678d27b7aec9cb21d715a87832835db
Jędrzej Niedzielski <60813479+ziomciopoziomcio@users.noreply.github.com>
2026-03-06 12:26:11 +0100

    Add interface and implementation for power calculation

--- zad4.py
+++ zad4.py
@@ -1,1 +1,11 @@
 from abc import ABC, abstractmethod
+
+class IArithmeticsPow(ABC):
+
+    @abstractmethod
+    def power(self, A: float, B: float) -> float:
+        pass
+class ArithmeticPow(IArithmeticsPow):
+
+    def power(self, A: float, B: float) -> float:
+        return A ** B
