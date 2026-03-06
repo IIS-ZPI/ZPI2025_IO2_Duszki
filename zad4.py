d552bcf2d2ee92965dfec1b8fc1dc25f2010a37e
Jerzy Andrusiewicz <jurek.andrusiewicz@gmail.com>
2026-03-06 12:29:15 +0100

    implemented ArithmeticMult

--- zad4.py
+++ zad4.py
@@ -1,1 +1,13 @@
 from abc import ABC, abstractmethod
+
+class IArithmeticsMult(ABC):
+
+    @abstractmethod
+    def multiplication(self, A: float, B: float) -> float:
+        pass
+
+class ArithmeticMult(IArithmeticsMult):
+
+    def multiplication(self, A: float, B: float) -> float:
+        return A * B
+    
