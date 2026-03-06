8fb938904ba265191d3731cde22ec078b1e528d3
Jerzy Andrusiewicz <jurek.andrusiewicz@gmail.com>
2026-03-06 12:47:41 +0100

    comment 3

--- zad4.py
+++ zad4.py
@@ -62,3 +62,4 @@ if __name__ == "__main__":
     print("Multiplication:", mult.multiplication(a, b))
     print("Division:", div.division(a, b))
     print("Power:", pow_op.power(a, b))
+    # Lab1_Task5_Marc311o_3
